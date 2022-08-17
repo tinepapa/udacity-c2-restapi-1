cb8e2d5950243b79249139e1aa5a5c4ca3655a57
tinepapa <tinembaye@hotmail.com>
2022-08-17 09:01:13 +0000

    new version with JWT

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,18 @@
+require('dotenv').config();
+
 export const config = {
     "dev": {
-      "username": "udagramtinedev",
-      "password": "udagramtine22",
-      "database": "udagramtinedev",
-      "host": "udagramtinedev.c9enzdgp18lg.us-east-1.rds.amazonaws.com",
-      "dialect": "postgres",
-      "aws_region": "us-east-1",
-      "aws_profile": "default",
-      "aws_media_bucket": "myaws729611696411"
+      "username": process.env.DEV_USERNAME,
+      "password": process.env.DEV_PASSWORD,
+      "database": process.env.DEV_DATABASE,
+      "host": process.env.DEV_DB_HOST,
+      "dialect": process.env.DEV_DIALECT,
+      "aws_region": process.env.DEV_AWS_REGION,
+      "aws_profile": process.env.DEV_AWS_PROFILE,
+      "aws_media_bucket": process.env.DEV_AWS_MEDIA_BUCKET
     },
     "jwt": {
-      "secret": " "
+      "secret": process.env.DEV_JWT_SECRET
     },
     "prod": {
       "username": "",
